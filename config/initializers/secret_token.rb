66725adc862dd6847e96bfe0437444ae3769840f
kgtong <kgtong1992@gmail.com>
2013-11-01 13:20:19 +0800

    完成宣传页面

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weber::Application.config.secret_token = '399ab51dccae60ba6caeaedb4e24d3095ad316d3e33ad46568979383dbb87e1767b6e36b1301485d6c67c23dccc4e2fc19e9f3fab75a27dadb577e587c5d0897'
